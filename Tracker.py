a788393b7b847a580966a1d32646557492d92c5b
inshal-qazi <muhammadinshaloffical@gmail.com>
2026-08-18 23:36:44 -0700

    exp-input system design

--- Tracker.py
+++ Tracker.py
@@ -1,1 +1,24 @@
-# i have add and commit the Tracker .py and only have to push after compltetaion
+#only have to push on github
+i=str(input("press (start or stop) to allow application to run:")).lower()
+if i=="start":
+    pass
+elif i=="stop":
+    print("no problem enter the expense when you remember")
+else: print("invalid input enter again")
+while i=="start":
+ a=str(input("enter on what thing have you expand:"))
+ if a =="stop":
+  break
+ b=int(input("enter thr price of thing"))
+
+
+
+
+
+
+
+
+
+
+
+
